5078d6ff8148a22d01a7fec66f9440074d502ffd
Antonio-Claudio Andelic <114903011+Balkaneros91@users.noreply.github.com>
2024-10-16 10:28:28 +0200

    Update question1.js

--- js/question1.js
+++ js/question1.js
@@ -2,6 +2,12 @@
     samt tilldela dem relevanta värden av relevant datatyp 
     Skriv ut värderna i consolen */
 
+let age = 32;
+let firstname = "Antonio-Claudio";
+let city = "Split";
+
+console.log("age", "firstname", "city");
+
 /* Vad är det för skillnad mellan let och const? */
 
    
